5f55451fce18d740db03f7fb6513b1aac990b87d
Shubham Kumar <hello@shubhkumar.in>
2020-01-07 13:57:26 +0530

    Added sequelize options alias.

--- src/Sequelize/Connection.js
+++ src/Sequelize/Connection.js
@@ -24,6 +24,43 @@
 */
 
 var Sequelize = require('sequelize');
+var Op = Sequelize.Op;
+const operatorsAliases = {
+  $eq: Op.eq,
+  $ne: Op.ne,
+  $gte: Op.gte,
+  $gt: Op.gt,
+  $lte: Op.lte,
+  $lt: Op.lt,
+  $not: Op.not,
+  $in: Op.in,
+  $notIn: Op.notIn,
+  $is: Op.is,
+  $like: Op.like,
+  $notLike: Op.notLike,
+  $iLike: Op.iLike,
+  $notILike: Op.notILike,
+  $regexp: Op.regexp,
+  $notRegexp: Op.notRegexp,
+  $iRegexp: Op.iRegexp,
+  $notIRegexp: Op.notIRegexp,
+  $between: Op.between,
+  $notBetween: Op.notBetween,
+  $overlap: Op.overlap,
+  $contains: Op.contains,
+  $contained: Op.contained,
+  $adjacent: Op.adjacent,
+  $strictLeft: Op.strictLeft,
+  $strictRight: Op.strictRight,
+  $noExtendRight: Op.noExtendRight,
+  $noExtendLeft: Op.noExtendLeft,
+  $and: Op.and,
+  $or: Op.or,
+  $any: Op.any,
+  $all: Op.all,
+  $values: Op.values,
+  $col: Op.col
+};
 
 exports._newSequelize = function (options) {
   return function () {
@@ -36,6 +73,7 @@ exports._newSequelize = function (options) {
         return options["logging"](m)();
       }
       newOpts.logging = loggerFn;
+      newOpts.operatorsAliases = operatorsAliases;
       return new Sequelize(newOpts);
     }
     return new Sequelize(options);
